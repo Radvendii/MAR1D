1e6d04ec248bf00cdd0b0f5cd0ea01f2c316ea54
Radvendii <dontsendmejunk@mailinator.com>
2014-12-28 16:48:31 -0500

    nobody reads these

--- objects.h
+++ objects.h
@@ -3,9 +3,10 @@
 #ifndef _OBJECT_H
 #define _OBJECT_H
 
-#define k_nPixels 100 //resolution of screen
+#define k_nPixels 500 //resolution of screen
 #define k_FOV 60 //Field of vision (degrees)
 #define k_nMaxLinesPerObj 30 //maximum number of lines per object
+#define k_drawD 500
 #define k_nMaxObj 50 //maximum number of objects in a world
 
 //labeling all the types of objects (enum)
@@ -14,16 +15,30 @@ enum objType {
     objGround
 };
 
+typdef struct {
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    char r;
+    char g;
+    char b;
+} line;
+
 
 //objects possible in the scene
-int g_ground[k_nMaxLinesPerObj*7] = { //two coordinates define a line (first four) and color (last three)
-    0,  16, 16, 16, 231, 95, 19,
-    16, 16, 16, 0,  231, 95, 19,
-    16, 0, 0, 0,    231, 95, 19,
-    0,  0, 0, 16,   231, 95, 19
-};
+static line *g_ground;
+static line *g_nothing;
 
-int g_nothing[k_nMaxLinesPerObj*7] = {};
+void g_init() {
+    g_ground = malloc(sizeof(line) * k_nMaxLinesPerObj);
+    g_ground[0] = (line) { .x1=0, .y1=16, .x2=16, .y2=16, .r=231, .g=95, .b=19 }
+    g_ground[1] = (line) { .x1=16, .y1=16, .x2=16, .y2=16, .r=231, .g=95, .b=19 }
+    g_ground[2] = (line) { .x1=16, .y1=0, .x2=0, .y2=0, .r=231, .g=95, .b=19 }
+    g_ground[3] = (line) { .x1=0, .y1=0, .x2=0, .y2=16, .r=231, .g=95, .b=19 }
+
+    g_nothing = malloc(sizeof(line) * k_nMaxLinesPerObj);
+}
 
 int *objFtype(enum objType type) {
     switch(type) {
@@ -40,7 +55,7 @@ int *objFtype(enum objType type) {
     }
 }
 
-const int g_levelTest[k_nMaxObj*3] = { //which object and coordinates
+const int g_levelTest[k_nMaxObj*3] = {
     objGround, 20, -8
 };
 
--- rendering.c
+++ rendering.c
@@ -2,69 +2,86 @@
 #include <math.h>
 #define pi (22/7.0)
 
-double intersectD(int *c_, int *l_){
-    double c[4];
-    double l[4];
-    double P[2];
-    for(int i=0;i<4;i++){c[i]=(double)c_[i]; l[i]=(double)l_[i];}
-    if(c_[0]==c_[2]) {
-        if(l_[0]==l_[2]) {return 0;}
-        P[0] = c[0];
-        P[1] = (l[3]-l[1])/(l[2]-l[0])*(c[0]-l[0])+l[1];
-    }
-    else if(l_[0]==l_[2]) {
-        P[0] = l[0];
-        P[1] = (c[3]-c[1])/(c[2]-c[0])*(l[0]-c[0])+c[1];
-    }
-    else if((l[3]-l[1])/(l[2]-l[0])==(c[3]-c[1])/(c[2]-c[0])){
-        return 0;
-    }
-    else {
-        double ml = (l[3]-l[1])/(l[2]-l[0]);
-        double mc = (c[3]-c[1])/(c[2]-c[0]);
-        P[0] = (ml*l[0] - mc*c[0] + c[1] - l[1]) / (ml-mc);
-        P[1] = (l[1]/ml - c[1]/mc + c[0] - l[0]) / (1/ml-1/mc);
-    }
-
-    if( !((P[0]<l[0] == P[0]>l[2]) && (P[1]<l[1] == P[1]>l[3]) && (c[2]>c[0] == P[0]>c[2]) && (c[3]>c[1] == P[1]>c[3])) ) {return 0;}
-
-    return pow(P[0]-c[0], 2)+pow(P[1]-c[1], 2);
-}
-
-
-int* renderTest(int *renderArr, int camX, int camY, int camT, int camD) { //returns an array of k_nPixels colors
-    double k0 = -cos(pi/180*(90-k_FOV/2.0-camT));
-    double k1 = camD/cos(pi/180*k_FOV/2.0)*cos(pi/180*camT);
-    double k2 = sin(pi/180*(90-k_FOV/2.0-camT));
-    double k3 = camD/cos(pi/180*k_FOV/2.0)*sin(pi/180*camT);
-    int camLine[4]; //Holds the ray eminating from the camera. Calculated based on what pixel we're on
-        camLine[0] = camX;
-        camLine[1] = camY;
-    int lineLine[4];
-    int closeD, newD;
+int* orthoTest(int *renderArr, int camX) {
+    int closeD;
+    int l[4];
     for(int y=0;y<k_nPixels;y++) {
-        double y_ = y*2*camD*tan(pi/180*k_FOV/2.0)/k_nPixels; //unit conversion from pixels to "true distance"
-        camLine[2] = (int)(camX+ y_*k0+k1);
-        camLine[3] = (int)(camY+ y_*k2+k3);
+        closeD = k_drawD;
         for(int obj=0;obj<k_nMaxObj;obj++) {
             for(int line=0;line<k_nMaxLinesPerObj;line++) {
-                closeD=500;
                 for(int i=0;i<4;i++){
-                    lineLine[i] = *(objFtype(g_levelTest[obj*3]) +line*7 +i)+g_levelTest[obj*3 +(i%2+1)]; //accesses the $line-th line of the object in question, and adds the location of that object to the lines' locations
+                    l[i] = *(objFtype(g_levelTest[obj*3]) +line*7 +i) + g_levelTest[obj*3 +(i%2+1)];
                 }
-/*if(y==250 && obj==0 && line==3){printf("%d, %d, %d, %d\n", camLine[0], camLine[1], camLine[2], camLine[3]);}*/
-                if((newD = intersectD(camLine, lineLine) ) == 0) {continue;}
-                if(newD < closeD) {
-                    closeD=newD;
-                    for(int i=0;i<3;i++) {
-                        renderArr[y*3+i]=*(objFtype(g_levelTest[obj*3]) +line*7+4+i);
-                    }
-                }
+                if(l[1]==l[3]){ continue; }
+                else{newD = y-l[1]-l[3]-l[1]
             }
         }
     }
-    return renderArr;
 }
+
+/*double intersectD(int *c_, int *l_){*/
+    /*double c[4];*/
+    /*double l[4];*/
+    /*double P[2];*/
+    /*for(int i=0;i<4;i++){c[i]=(double)c_[i]; l[i]=(double)l_[i];}*/
+    /*if(c_[0]==c_[2]) {*/
+        /*if(l_[0]==l_[2]) {return 0;}*/
+        /*P[0] = c[0];*/
+        /*P[1] = (l[3]-l[1])/(l[2]-l[0])*(c[0]-l[0])+l[1];*/
+    /*}*/
+    /*else if(l_[0]==l_[2]) {*/
+        /*P[0] = l[0];*/
+        /*P[1] = (c[3]-c[1])/(c[2]-c[0])*(l[0]-c[0])+c[1];*/
+    /*}*/
+    /*else if((l[3]-l[1])/(l[2]-l[0])==(c[3]-c[1])/(c[2]-c[0])){*/
+        /*return 0;*/
+    /*}*/
+    /*else {*/
+        /*double ml = (l[3]-l[1])/(l[2]-l[0]);*/
+        /*double mc = (c[3]-c[1])/(c[2]-c[0]);*/
+        /*P[0] = (ml*l[0] - mc*c[0] + c[1] - l[1]) / (ml-mc);*/
+        /*P[1] = (l[1]/ml - c[1]/mc + c[0] - l[0]) / (1/ml-1/mc);*/
+    /*}*/
+
+    /*if( !((P[0]<l[0] == P[0]>l[2]) && (P[1]<l[1] == P[1]>l[3]) && (c[2]>c[0] == P[0]>c[2]) && (c[3]>c[1] == P[1]>c[3])) ) {return 0;}*/
+
+    /*return pow(P[0]-c[0], 2)+pow(P[1]-c[1], 2);*/
+/*}*/
+
+
+/*int* renderTest(int *renderArr, int camX, int camY, int camT, int camD) { //returns an array of k_nPixels colors*/
+    /*double k0 = -cos(pi/180*(90-camT));*/
+    /*double k1 = camD/cos(pi/180*k_FOV/2.0)*cos(pi/180*(camT+k_FOV/2.0));*/
+    /*double k2 = sin(pi/180*(90-camT));*/
+    /*double k3 = camD/cos(pi/180*k_FOV/2.0)*sin(pi/180*(camT+k_FOV/2.0));*/
+    /*int camLine[4]; //Holds the ray eminating from the camera. Calculated based on what pixel we're on*/
+        /*camLine[0] = camX;*/
+        /*camLine[1] = camY;*/
+    /*int lineLine[4];*/
+    /*int closeD, newD;*/
+    /*for(int y=0;y<k_nPixels;y++) {*/
+        /*double y_ = y*2*camD*tan(pi/180*k_FOV/2.0)/k_nPixels; //unit conversion from pixels to "true distance"*/
+        /*camLine[2] = (int)(camX+ y_*k0+k1);*/
+        /*camLine[3] = (int)(camY+ y_*k2+k3);*/
+        /*closeD=k_drawD;*/
+        /*for(int obj=0;obj<k_nMaxObj;obj++) {*/
+            /*for(int line=0;line<k_nMaxLinesPerObj;line++) {*/
+                /*for(int i=0;i<4;i++){*/
+                    /*lineLine[i] = *(objFtype(g_levelTest[obj*3]) +line*7 +i)+g_levelTest[obj*3 +(i%2+1)]; //accesses the $line-th line of the object in question, and adds the location of that object to the lines' locations*/
+                /*}*/
+/*[>if(y==250 && obj==0 && line==3){printf("%d, %d, %d, %d\n", camLine[0], camLine[1], camLine[2], camLine[3]);}<]*/
+                /*if((newD = intersectD(camLine, lineLine) ) == 0) {continue;}*/
+                /*if(newD < closeD) {*/
+                    /*closeD=newD;*/
+                    /*for(int i=0;i<3;i++) {*/
+                        /*renderArr[y*3+i]=*(objFtype(g_levelTest[obj*3]) +line*7+4+i);*/
+                    /*}*/
+                /*}*/
+            /*}*/
+        /*}*/
+    /*}*/
+    /*return renderArr;*/
+/*}*/
 
 int main(int argc, char **argv) {
     /*int c[4];*/
@@ -73,7 +90,7 @@ int main(int argc, char **argv) {
     /*printf("%f", intersectD(c, l));*/
 
     int rendered[k_nPixels*3] = {};
-    int *renderptr = renderTest(rendered, 0,0,-30,10);
+    int *renderptr = renderTest(rendered, 0,0,0,10);
     for(int i=0;i<k_nPixels;i++) {
         printf("%d\n", *(renderptr+i*3));
     }
