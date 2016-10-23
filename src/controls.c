6550ee7bddeaa6d15b0f7b5aa061840e84847bc6
Taeer Bar-Yam <taeer@necsi.edu>
2016-10-23 09:53:22 -0400

    pause audio

--- src/controls.c
+++ src/controls.c
@@ -196,6 +196,12 @@ void cl_keypress(int key, int scancode, int action, int mods){
   if (key == GLFW_KEY_Q && action == GLFW_PRESS){
     s.paused = !s.paused;
     s.userPaused = !s.userPaused;
+    if(s.paused){
+      au_mainStop();
+    }
+    else{
+      au_mainPlay(au_mainAudio);
+    }
   }
 }
 
