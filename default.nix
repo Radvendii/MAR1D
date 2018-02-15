882a9206728f5d393b1348421380967f0a18c361
Taeer Bar-Yam <taeer@necsi.edu>
2018-02-15 11:42:02 -0500

    nix compilation in place

---
+++ default.nix
@@ -0,0 +1,56 @@
+with import <nixpkgs> { };
+with xorg;
+
+let 
+  width = 30;
+  mute = false;
+  effects = false;
+  sensitivity = 5;
+  reverseY = false;
+in
+
+stdenv.mkDerivation rec {
+  name = "MAR1D-${version}";
+  version = "0.2.0";
+  options = "-w${toString width}"
+          + " -s${toString sensitivity}"
+          + (if mute then " -m" else "")
+          + (if effects then " -f" else "")
+          + (if reverseY then " -r" else "");
+
+  src = ./.;
+
+  buildInputs =
+    [
+      alsaLib
+      alsaOss
+      cmake
+      doxygen
+      libao
+      libpthreadstubs
+      mesa_glu
+      x11
+      xinput_calibrator
+      xorg.libXrandr
+      xorg.libXi
+      xorg.xinput
+    ];
+
+  preConfigure = ''
+    cd src
+  '';
+
+  meta = with stdenv.lib; {
+    description = "First person Super Mario Bros";
+    longDescription = ''
+      The original Super Mario Bros as you've never seen it. Step into Mario's
+      shoes in this first person clone of the classic Mario game. True to the
+      original, however, the game still takes place in a two dimensional world.
+      You must view the world as mario does, as a one dimensional line.
+    '';
+    homepage = https://github.com/olynch/fp_mario;
+    license = licenses.agpl3;
+    maintainers = with maintainers; [ taeer ];
+    platforms = platforms.linux;
+  };
+}
