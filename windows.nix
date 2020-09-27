00894c4f7c54f47eac2cd5ecbdfa8458e823568c
Taeer Bar-Yam <taeer@necsi.edu>
2020-09-26 23:14:46 -0700

    refactor .nix files. package.nix can be used directly in nixpkgs

--- windows.nix
+++ windows.nix
@@ -73,36 +73,5 @@ with import <nixpkgs> {
     })
   ];
 };
-callPackage (
-  { stdenv, SDL2, SDL2_mixer, libGLU, libconfig, cmake }:
-  stdenv.mkDerivation rec {
-    name = "MAR1D-${version}";
-    version = "0.2.0";
 
-    src = ./.;
-
-    nativeBuildInputs = [ cmake ];
-
-    buildInputs =
-      [
-        SDL2
-        SDL2_mixer
-        libGLU
-        libconfig
-      ];
-
-    meta = with stdenv.lib; {
-      description = "First person Super Mario Bros";
-      longDescription = ''
-        The original Super Mario Bros as you've never seen it. Step into Mario's
-        shoes in this first person clone of the classic Mario game. True to the
-        original, however, the game still takes place in a two dimensional world.
-        You must view the world as mario does, as a one dimensional line.
-      '';
-      homepage = https://github.com/olynch/fp_mario;
-      license = licenses.agpl3;
-      maintainers = with maintainers; [ taeer ];
-      platforms = [ platforms.linux platforms.windows ];
-    };
-  }
-) {}
+callPackage ./package.nix {}
