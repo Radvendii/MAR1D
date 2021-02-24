668d6b5a806757fe5947c6586430de33e08bddc9
Taeer Bar-Yam <taeer@necsi.edu>
2021-02-23 22:33:51 -0500

    refactor website

--- website/site.hs
+++ website/site.hs
@@ -1,5 +1,14 @@
-import           Hakyll
-import           Hakyll.Web.Sass
+import Hakyll
+import Hakyll.Web.Sass
+import System.FilePath.Posix (takeBaseName, takeDirectory, (</>))
+
+-- Moves from `/path/to/source.ext` to `/path/to/source/index.html`
+-- This way the page can be accessed by `website.tld/path/to/source` without any extension
+cleanRoute :: Routes
+cleanRoute = customRoute createIndexRoute
+  where
+    createIndexRoute ident = takeDirectory p </> takeBaseName p </> "index.html"
+      where p = toFilePath ident
 
 main :: IO ()
 main = hakyll $ do
@@ -11,11 +20,18 @@ main = hakyll $ do
         route $ setExtension "css"
         compile $ (compressCss <$>) <$> sassCompiler
 
-    match (fromList ["index.html", "downloads/index.html"]) $ do
+    match "index.html" $ do
         route   $ idRoute
-        compile $ pandocCompiler
+        compile $ do
+          getResourceBody
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
+    match "*.html" $ do
+      route $ cleanRoute
+      compile $ do
+        getResourceBody
+          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+          >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
