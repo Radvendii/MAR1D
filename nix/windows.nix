7cc2b6f3d5bac373c67e67aca866b554c43fe67d
Taeer Bar-Yam <taeer.bar-yam@moduscreate.com>
2024-05-10 17:51:08 -0400

    nix build .#windows works

--- nix/windows.nix
+++ nix/windows.nix
@@ -9,9 +9,20 @@
 with import ./windows-pkgs.nix { inherit nixpkgs system overlays; };
 
 (callPackage ./package.nix {}).overrideAttrs (old: {
+  nativeBuildInputs = old.nativeBuildInputs ++ [(
+    # somebody's done something wrong here. I don't know if zig should be
+    # looking for pkg-config somewhere else, or if nix should be providing
+    # a pkg-config binary with the appropriate name. Either way this is a
+    # compatibility hack
+    # TODO: work it out upstream
+    buildPackages.runCommand "rename-pkg-config" {} ''
+      mkdir -p $out/bin
+      ln -s ${buildPackages.pkg-config}/bin/x86_64-w64-mingw32-pkg-config $out/bin/pkg-config
+    ''
+  )];
+  dontAutoPatchelf = true;
   NIX_LDFLAGS = "-lssp";
   zigFlags = old.zigFlags ++ [
-    # $prefix doesn't get expanded here
     "--prefix-exe-dir ."
     "-Ddatadir=resources"
     "-Dportable=true"
